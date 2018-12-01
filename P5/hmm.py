421972c17c884c59d652a7bda070bc9d19cc0359
Pratheek Bhat <pbhat@linkedin.com>
2018-12-01 15:59:18 -0800

    hmm.py viterbi 10

--- P5/hmm.py
+++ P5/hmm.py
@@ -125,11 +125,10 @@ def viterbi(pi, A, B, O):
     delta[s, 0] = pi[s] * B[s, O[0]]
   for t in range(1, N):
     for s in range(S):
-      delta_args = [A[s_prime, s] * delta[s_prime, t-1] * B[s, O[t]] for s_prime in range(S)]
-      delta[s, t] = max(delta_args)
+      delta_args = [A[s_prime, s] * delta[s_prime, t-1] for s_prime in range(S)]
+      delta[s, t] = B[s, O[t]] * max(delta_args)
       delta_uppercase[s, t] = np.argmax(delta_args)
-  path.append(np.argmax(delta[:, -1]))
-  print(path)
+  path.append(np.argmax(delta[:, N-1]))
   for t in reversed(range(N - 1)):
     path.append(delta_uppercase[path[-1], t])
   path = reversed(path)
